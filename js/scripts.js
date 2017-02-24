1748e10e766bf41952730d75f81143b30533d159
likewater <lakebayomackway@gamil.com>
2017-02-24 12:42:04 -0800

    add content to array numberList in js file.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,37 @@ $(function() {
   var count = function countNumbers(counter) {
     //alert(counter);
     for(i = 1; i <= counter; i++) {
+
+      // if (i % 3 === 0) {
+      //   i = "ping";
+      // } else if (i % 6 === 0) {
+      //   i = "pong";
+      // } else if (i % 15 === 0) {
+      //   i = "ping-pong";
+      // } else {
+      //   continue;
+      // }
+
+
+      // if (i % 3 === 0) {
+      //   i = "ping";
+      // } //else
+
+      // if (i % 5 === 0) {
+      //   i = "pong";
+      // } //else
+
+      // if (i % 15 === 0) {
+      //   i = "ping-pong";
+      // }
+
+      // if (i % 5 === 0) {
+      //   i = "pong";
+      // }
+      numberList.push(i);
+      console.log(numberList);
       $("#listOutput").append('<li>' + i + '</li>');
+
     }
     result = i;
     return result;
