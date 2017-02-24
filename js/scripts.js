05526b7a67cac8c0ca285312006cdf312648ed0e
likewater <lakebayomackway@gamil.com>
2017-02-24 14:47:50 -0800

    remove bloated crappy script.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,18 @@
 //backend logic
 $(function() {
   var numberList = [];
-  var alteredList = [];
   //count function
   var count = function countNumbers(counter) {
-    //alert(counter);
     for(i = 1; i <= counter; i++) {
-
-      // if (i % 3 === 0) {
-      //   i = "ping";
-      // } else if (i % 6 === 0) {
-      //   i = "pong";
-      // } else if (i % 15 === 0) {
-      //   i = "ping-pong";
-      // } else {
-      //   continue;
-      // }
-
-
-      // if (i % 3 === 0) {
-      //   i = "ping";
-      // } //else
-
-      // if (i % 5 === 0) {
-      //   i = "pong";
-      // } //else
-
-      // if (i % 15 === 0) {
-      //   i = "ping-pong";
-      // }
-
-      // if (i % 5 === 0) {
-      //   i = "pong";
-      // }
-      numberList.push(i);
-      console.log(numberList);
       $("#listOutput").append('<li>' + i + '</li>');
-
+      numberList.push(i);
+      numberList.forEach(function(i){
+      // if (i % 5 === 0){
+      //   i = "pong";
+      //   }
+      });
     }
-    result = i;
-    return result;
+    console.log(numberList);
   }
 
 //front end logic
