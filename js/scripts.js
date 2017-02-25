5a485573df7a77800e18802cbe83903c3e8691ac
likewater <lakebayomackway@gamil.com>
2017-02-24 16:25:57 -0800

    comment out problen forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,17 @@ $(function() {
   //count function
   var count = function countNumbers(counter) {
     for(i = 1; i <= counter; i++) {
-      $("#listOutput").append('<li>' + i + '</li>');
+      //$("#listOutput").append('<li>' + i + '</li>');
       numberList.push(i);
-      changeList.forEach(function(i){
-      if (i % 3 === 0) {
-        i = "ping";
-        }
-      });
+      // numberList.forEach(function(i){
+      //   var ping = i % 3 === 0
+      // if (ping) {
+      //   i = "ping";
+      // } else if (i % 3 !== 0) {
+      //
+      // }
+      // });
+      $("#listOutput").append('<li>' + i + '</li>');
     }
     console.log(numberList);
     result = numberList;
