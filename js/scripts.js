9dc9cc7f44c5f02801d530ca8534b3348f3d7fc4
likewater <lakebayomackway@gamil.com>
2017-02-24 16:10:15 -0800

    add alignment to js and html files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
 //backend logic
 $(function() {
   var numberList = [];
-  var changeList = [];
+
   //count function
   var count = function countNumbers(counter) {
-
-
     for(i = 1; i <= counter; i++) {
-
-
       $("#listOutput").append('<li>' + i + '</li>');
       numberList.push(i);
       changeList.forEach(function(i){
       if (i % 3 === 0) {
         i = "ping";
-      }
-    });
+        }
+      });
     }
-
     console.log(numberList);
     result = numberList;
     return result;
   }
 
 //front end logic
-    $("#formFlow").submit(function() {
-      event.preventDefault();
-      var userInput = $("#textInput").val();
-      result = count(userInput);
+  $("#formFlow").submit(function() {
+    event.preventDefault();
+    var userInput = $("#textInput").val();
+    result = count(userInput);
     //$("#listOutput").text(result);
-    });
   });
+});
